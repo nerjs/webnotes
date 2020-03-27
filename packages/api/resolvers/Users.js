2956e9312771ff048a392bfae3826e5d03f765cd
nerjs <karifan.stap@gmail.com>
2020-03-27 17:46:48 +0200

    user schema gql

--- packages/api/resolvers/Users.js
+++ packages/api/resolvers/Users.js
@@ -1,6 +1,26 @@
-module.exports = {
-    Query: {
-        user: () => ({ name: 'test' }),
-        users: () => [],
+const { Users } = require('@nbs/db')
+
+const Query = {
+    user: async (_, { id }) => {
+        return Users.findById(id)
     },
+    users: () => Users.find(),
+}
+
+const Mutation = {
+    addUser: async (_, { user }) => {
+        const createdUser = new Users(user)
+        await createdUser.save()
+        return createdUser
+    },
+    editUser: async (_, { id, user }) => Users.findByIdAndUpdate(id, user, { new: true }),
+    deleteUser: async (_, { id }) => {
+        await Users.findByIdAndDelete(id)
+        return true
+    },
+}
+
+module.exports = {
+    Query,
+    Mutation,
 }
