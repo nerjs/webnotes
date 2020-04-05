776160645158e282ceddccb43f5c049f798637f5
nerjs <karifan.stap@gmail.com>
2020-04-05 17:03:16 +0300

    add redis & api RedisPubSub

--- packages/api/lib/pubsub.js
+++ packages/api/lib/pubsub.js
@@ -1,5 +1,16 @@
-const { PubSub } = require('graphql-subscriptions')
+const { RedisPubSub } = require('graphql-redis-subscriptions')
 
-const pubsub = new PubSub()
+const { REDIS_HOST, REDIS_PORT } = process.env
+
+const pubsub = new RedisPubSub({
+    connection: {
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+        retryStrategy: times => {
+            // reconnect after
+            return Math.min(times * 50, 2000)
+        },
+    },
+})
 
 module.exports = pubsub
