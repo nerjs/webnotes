bb0fdda9467761d91796f392d0a6e17b836f2b69
nerjs <karifan.stap@gmail.com>
2020-04-08 22:07:16 +0300

    client: edit users profile

--- packages/client/src/routes/User.js
+++ packages/client/src/routes/User.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
 import PageWrapper from 'components/wrappers/PageWrapper'
+import UserSidebar from 'components/userSeidebar'
+import NoteList from 'components/noteList'
+import useUserLoginToId from 'hooks/useUserLoginToId'
+import Alert from '@material-ui/lab/Alert'
+import LinearProgress from '@material-ui/core/LinearProgress'
 
-const UserRoute = () => {
-    return <PageWrapper>User</PageWrapper>
+const UserRoute = ({ match: { params } }) => {
+    const { loading, error, userId } = useUserLoginToId(params)
+
+    return (
+        <PageWrapper>
+            {loading && <LinearProgress />}
+            {!loading && (error || !userId) && (
+                <Alert severity="error">{(error && error.message) || 'User not found'}</Alert>
+            )}
+            {userId && (
+                <Grid container spacing={1}>
+                    <Grid item md={3}>
+                        <UserSidebar userId={userId} />
+                    </Grid>
+                    <Grid item md={9}>
+                        <NoteList userId={userId} />
+                    </Grid>
+                </Grid>
+            )}
+        </PageWrapper>
+    )
 }
 
 export default UserRoute
--- packages/client/src/routes/Users.js
+++ packages/client/src/routes/Users.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
 import PageWrapper from 'components/wrappers/PageWrapper'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Alert from '@material-ui/lab/Alert'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import gql from 'graphql-tag'
+import { Link } from 'react-router-dom'
+import { userRoute } from 'helpers/routes'
+
+const ListItemLink = props => <ListItem button component={Link} {...props} />
+
+const GET_USERS = gql`
+    query {
+        allUsers: users {
+            id
+            login
+        }
+    }
+`
 
 const UsersRoute = () => {
-    return <PageWrapper>userS</PageWrapper>
+    const { error, loading, data } = useQuery(GET_USERS)
+
+    return (
+        <PageWrapper>
+            {loading && <LinearProgress />}
+            {error && <Alert severity="error">{error.message}</Alert>}
+            {!error && data && data.allUsers && (
+                <List>
+                    {data.allUsers.map(({ id, login }) => (
+                        <ListItemLink key={id} to={userRoute.link(id)}>
+                            {login}
+                        </ListItemLink>
+                    ))}
+                </List>
+            )}
+        </PageWrapper>
+    )
 }
 
 export default UsersRoute
