9261c7edbf5e722bf5d2979c265c9be30f63629f
nerjs <karifan.stap@gmail.com>
2020-04-19 19:42:54 +0300

    client: change MAkeTabs with new icons list

--- packages/client/src/components/makeNotes/Tabs.js
+++ packages/client/src/components/makeNotes/Tabs.js
@@ -1,16 +1,13 @@
 import React, { useContext } from 'react'
-import { NOTE_TYPES, NOTE_TYPES_ARR } from '@nbs/global'
+import { NOTE_TYPES_ARR } from '@nbs/global'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
-import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder'
-import TextFieldsIcon from '@material-ui/icons/TextFields'
-import LinkIcon from '@material-ui/icons/Link'
 import { MakeNotesContext } from './ctx'
+import kindIcons from '../icons/formKind'
 
-const icons = {
-    [NOTE_TYPES.DIR]: <CreateNewFolderIcon />,
-    [NOTE_TYPES.TEXT]: <TextFieldsIcon />,
-    [NOTE_TYPES.LINK]: <LinkIcon />,
+const tabIcon = kind => {
+    const TIcon = kindIcons[kind]
+    return <TIcon />
 }
 
 const MakeNotesTabs = () => {
@@ -27,8 +24,8 @@ const MakeNotesTabs = () => {
             {NOTE_TYPES_ARR.map(nt => (
                 <Tab
                     key={nt}
-                    label={icons[nt] ? undefined : nt}
-                    icon={icons[nt]}
+                    label={tabIcon(nt) ? undefined : nt}
+                    icon={tabIcon(nt)}
                     value={nt}
                     disabled={blockedKind.has(nt)}
                 />
