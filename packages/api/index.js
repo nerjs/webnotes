6c5bf6208405c9de051b101f49cd01bc92d67658
nerjs <nerjs.stap@gmail.com>
2020-05-06 23:22:17 +0300

    workflow: remove test errors & master workflow

--- packages/api/index.js
+++ packages/api/index.js
@@ -11,6 +11,4 @@ gqlApp.listen(Number(API_SERVER_PORT), err => {
     if (err) return logger.error(err)
 
     logger.log('Start API server: ', `http://localhost:${API_SERVER_PORT}`)
-
-    setTimeout(() => bla(), 30000)
 })
--- scripts/check_containers.sh
+++ scripts/check_containers.sh
@@ -5,7 +5,6 @@ EXITED_CONTAINERS=$(docker-compose ps | grep Exit)
 echo $EXITED_CONTAINERS
 
 if [ "$EXITED_CONTAINERS" ]; then
-    echo "One or more containers were stopped unexpectedly" >&2
     exit 1
 else
     exit 0
