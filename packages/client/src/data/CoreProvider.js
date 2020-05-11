630af55d69e19cd5f06dccbf6f2525f3dacbbb67
nerjs <nerjs.stap@gmail.com>
2020-05-11 20:26:59 +0300

    client: dynamic Router

--- packages/client/src/data/CoreProvider.js
+++ packages/client/src/data/CoreProvider.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import GqlDataProvider from './GqlDataProvider'
 import AuthProvider from './Auth'
 import ErrorHandlingProvider from './ErrorHandling'
+
+const Router = process.env.REACT_APP_HASH_ROUTER ? HashRouter : BrowserRouter
 
 const CoreProvider = ({ children }) => {
     return (
         <GqlDataProvider>
             <AuthProvider>
-                <BrowserRouter basename="/">{children} </BrowserRouter>
+                <Router basename="/">{children} </Router>
             </AuthProvider>
             <ErrorHandlingProvider />
         </GqlDataProvider>
