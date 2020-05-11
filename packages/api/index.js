9272e6b0979e85c7eb9a2acd0bb0d6c8c4e23b3c
nerjs <nerjs.stap@gmail.com>
2020-05-11 18:48:33 +0300

    workflow,api: fix api port

--- packages/api/index.js
+++ packages/api/index.js
@@ -2,13 +2,14 @@ require('./lib/config')
 const logger = require('nlogs')(module)
 const gqlApp = require('./app')
 
-const { API_SERVER_PORT } = process.env
+const { API_SERVER_PORT, API_SERVER_APP_PORT, API_SERVER_HOST } = process.env
+const PORT = Number(API_SERVER_APP_PORT || API_SERVER_PORT)
 
 console.log('\n')
 logger.log('Try start server')
 
-gqlApp.listen(Number(API_SERVER_PORT), err => {
+gqlApp.listen(PORT, err => {
     if (err) return logger.error(err)
 
-    logger.log('Start API server: ', `http://localhost:${API_SERVER_PORT}`)
+    logger.log('Start API server: ', `http://${API_SERVER_HOST}:${PORT}`)
 })
