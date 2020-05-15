798b243a009ad2e252beb41016c93a2c6a70cd37
nerjs <nerjs.stap@gmail.com>
2020-05-15 13:27:42 +0300

    client: fix gql paths

--- packages/client/src/data/GqlDataProvider.js
+++ packages/client/src/data/GqlDataProvider.js
@@ -8,8 +8,17 @@ const {
     REACT_APP_SUBSCRIBE_SERVER_PATH,
 } = process.env
 
-const urlGql = `${window.location.protocol}//${REACT_APP_API_SERVER_HOST}:${REACT_APP_API_SERVER_PORT}${REACT_APP_API_SERVER_PATH}`
-const wsUrlGql = `ws://${REACT_APP_API_SERVER_HOST}:${REACT_APP_API_SERVER_PORT}${REACT_APP_SUBSCRIBE_SERVER_PATH}`
+const { urlGql, wsUrlGql } = (() => {
+    const wsPort = window.location.protocol.includes('https') ? 'wss:' : 'ws:'
+    const gqlPort = window.location.protocol
+
+    const port = REACT_APP_API_SERVER_PORT ? `:${REACT_APP_API_SERVER_PORT}` : ''
+
+    return {
+        urlGql: `${gqlPort}//${REACT_APP_API_SERVER_HOST}${port}${REACT_APP_API_SERVER_PATH}`,
+        wsUrlGql: `${wsPort}//${REACT_APP_API_SERVER_HOST}${port}${REACT_APP_SUBSCRIBE_SERVER_PATH}`,
+    }
+})()
 
 const GqlDataProvider = ({ children }) => {
     return (
